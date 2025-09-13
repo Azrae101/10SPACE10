e878a46eeddbf4139a41a21a69b91bf39c24a1b6
Clara <clar.holst@gmail.com>
2025-09-13 14:34:19 +0200

    room 9

--- app.py
+++ app.py
@@ -6,33 +6,45 @@ app = Flask(__name__)
 def index():
     return render_template('room1.html')
 
-@app.route('/room1')
+@app.route('/1')
 def room1():
     return render_template('room1.html')
 
-@app.route('/room2') # Room 4 in notes
+@app.route('/2') 
 def room2():
     return render_template('room2.html')
 
-@app.route('/room3')
+@app.route('/3')
 def room3():
     return render_template('room3.html')
 
-@app.route('/room4')
+@app.route('/4')
 def room4():
     return render_template('room4.html')
 
-@app.route('/room6')
+@app.route('/5')
+def room5():
+    return render_template('room5.html')
+
+@app.route('/6')
 def room6():
     return render_template('room6.html')
 
-@app.route('/room7')
+@app.route('/7')
 def room7():
     return render_template('room7.html')
 
-@app.route('/room8')
+@app.route('/8')
 def room8():
     return render_template('room8.html')
+
+@app.route('/9')
+def room9():
+    return render_template('room9.html')
+
+@app.route('/10')
+def room10():
+    return render_template('room10.html')
 
 # For local testing only:
 if __name__ == '__main__':
